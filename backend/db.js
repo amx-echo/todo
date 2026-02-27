3312c42540d9087da5ea3e10fd589d47a2fd1b9d
amx-echo <shivam.ashtikar@ltimindtree.com>
2026-02-27 14:51:30 +0530

    db update

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
     user: process.env.USER,
     password:process.env.PASSWORD,
     host: process.env.HOST,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     database: process.env.DB
 })
 
